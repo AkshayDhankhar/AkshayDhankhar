09849a7d4d54a283e472ae5c88e4ea698e844dc4
DevsMitra <rahulshrm2109@gmail.com>
2022-03-07 18:09:19 +0530

    testing

---
+++ _next/static/XM9QYAjNQz9Q2kLm3K3et/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-17d32c3408328e2e.js"],"/_error":["static/chunks/pages/_error-2f883067a14f4c4a.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
